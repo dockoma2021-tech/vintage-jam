548a5a3a540ea8f11927cfd29743e69d2b69e5d1
Mykola <dockoma2021@gmail.com>
2026-08-01 01:57:54 +0300

    Publish vj-000003 from Admin 3.3

--- data/catalog-data.js
+++ data/catalog-data.js
@@ -1,6 +1,6 @@
 window.VINTAGE_JAM_DATA = {
   "version": "3.3.0",
-  "generatedAt": "2026-07-31T22:57:41.888Z",
+  "generatedAt": "2026-07-31T22:57:54.695Z",
   "site": {
     "name": "Vintage Jam",
     "defaultLanguage": "uk"
